f6a158713207e1f837cd4cdbc5a4abd8a2a22d94
pankajsgote16 <pankajsgote01@gmail.com>
2023-10-26 13:28:33 +0530

    Commit#13
    Hard coded values hold in constants and reused.

---
+++ PGSpringApp/src/main/java/com/blogapp/config/AppConstants.java
@@ -0,0 +1,13 @@
+package com.blogapp.config;
+/*
+ * All constants are stored in this class and this is to be good practice.
+ * No hard coded value should be used in application as it is called to be bad practice.
+ */
+public class AppConstants {
+
+	public static final String PAGE_NUMBER = "0";
+	public static final String PAGE_SIZE = "10";
+	public static final String SORT_BY = "postId";
+	public static final String SORT_DIR = "asc";
+	
+}
